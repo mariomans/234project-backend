1e5f897c9271494c20d144dcf4fccecdd5d1c502
Unknown <frong_98_@hotmail.com>
2018-05-01 18:29:01 +0700

    Add information user in Database in class userDaoImpl

--- src/main/java/camt/se234/project/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/project/dao/UserDaoImpl.java
@@ -5,10 +5,19 @@ import camt.se234.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class UserDaoImpl implements UserDao{
+    List<User> users;
     @Autowired
     UserRepository userRepository;
+
+    public UserDaoImpl(){
+        users = new ArrayList<>();
+        users.add(new User("001","mario123"));
+    }
     @Override
     public User getUser(String username, String password) {
         return userRepository.findByUsernameAndPassword(username,password);
