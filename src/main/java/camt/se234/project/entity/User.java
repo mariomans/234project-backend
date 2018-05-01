efb3fc4f2f0e8f8bb7944614fa94e7b13eb615c4
Unknown <frong_98_@hotmail.com>
2018-05-01 18:28:36 +0700

    Create to recieve constructure username password and role
    In class User

--- src/main/java/camt/se234/project/entity/User.java
+++ src/main/java/camt/se234/project/entity/User.java
@@ -22,4 +22,29 @@ public class User {
     String username;
     String password;
     String role;
-}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    }
