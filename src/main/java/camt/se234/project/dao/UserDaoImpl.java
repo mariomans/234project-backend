9001529991dec0d5009be9ebd3906de2ce0b1a96
Unknown <frong_98_@hotmail.com>
2018-05-10 19:24:19 +0700

    Add constructor to UserDaoImpl

--- src/main/java/camt/se234/project/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/project/dao/UserDaoImpl.java
@@ -17,4 +17,9 @@ public class UserDaoImpl implements UserDao{
     public User getUser(String username, String password) {
         return userRepository.findByUsernameAndPassword(username,password);
     }
+
+    @Override
+    public User getRole() {
+        return null;
+    }
 }
