dd7c34f5df910ae451ccfe1cd5a6278de6e4d055
Unknown <frong_98_@hotmail.com>
2018-05-10 19:22:09 +0700

    Covert UserDaoImpl.java class
    to the old class

--- src/main/java/camt/se234/project/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/project/dao/UserDaoImpl.java
@@ -10,14 +10,9 @@ import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao{
-    List<User> users;
     @Autowired
     UserRepository userRepository;
 
-    public UserDaoImpl(){
-        users = new ArrayList<>();
-        users.add(new User("001","mario123"));
-    }
     @Override
     public User getUser(String username, String password) {
         return userRepository.findByUsernameAndPassword(username,password);
